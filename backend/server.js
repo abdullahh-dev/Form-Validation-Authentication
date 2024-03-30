d60061b79a70e2e2e6736cbb1a1cf13115647bd4
Abdullah <abdullahaha101@gmail.com>
2024-03-30 15:46:42 +0500

    created new backend folders

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const db = require('mongoose');
 const cors = require('cors');
 const PORT = process.env.PORT;
+const DB_URL = process.env.DB;  
 
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Listening'); 
-});
-
-app.post('/api/users/createUser', (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is Live on PORT ${PORT}`);
